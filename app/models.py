5258f1bf53c33a6a31b0c044ddd316908c0af75a
Maryan23 <maryann.mwikali@student.moringaschool.com>
2022-01-20 16:32:42 +0300

    Created class profile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    bio = models.TextField(max_length=100)
+    f_name = models.CharField(max_length=20)
+    tel_number = models.CharField(max_length=14)
