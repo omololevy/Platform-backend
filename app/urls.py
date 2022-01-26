771321240789e03e0bceac03f2dab1dbf5475241
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-26 10:40:14 +0300

    Create thee router and the url pattern  for the public cohort.

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,13 @@ from rest_framework.authtoken.views import ObtainAuthToken
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'profile', views.UserProfileViewSet)
+router.register(r'public-cohort',views.PublicCohortViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', ObtainAuthToken.as_view()),
-    path('profile/',views.UserProfileSerializer)
+    path('profile/',views.UserProfileSerializer),
+    path('public-cohort/',views.PublicCohortSerializer),
 ]
