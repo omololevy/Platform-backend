d95087b7663c9a6363a4d89dabfc0220839b50f8
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2022-01-27 10:35:35 +0300

    cohort

--- app/serializers.py
+++ app/serializers.py
@@ -12,8 +12,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
 
-        # print(validated_data)
-        # print('Hello')
+      
         return user
 
 
