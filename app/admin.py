deb726c5a84f33079ab9221271a2530b26aaa58a
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-28 11:24:39 +0300

    Adjust admin file and discard changes

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 from .models import privateCohort
 from .models import PublicCohort
+from .models import Profile
 # Register your models here.
 
 admin.site.register(privateCohort)
 admin.site.register(PublicCohort)
+admin.site.register(Profile)
