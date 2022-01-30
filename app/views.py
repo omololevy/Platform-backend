8af99bab0a96ee2c06142c7389d690e858ad8392
Maryan23 <maryann.mwikali@student.moringaschool.com>
2022-01-30 23:49:12 +0300

    Adds the custom auth token that returns user id

--- app/views.py
+++ app/views.py
@@ -10,6 +10,23 @@ from rest_framework.response import Response
 from rest_framework import status,generics
 # authentication
 from django.contrib.auth import authenticate, login, logout
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
