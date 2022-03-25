ae7531015579564b374d0f0a73c90d0e57951595
omololevy <cotechlevy@gmail.com>
2022-03-25 11:14:01 +0300

    Allowed hosts

--- Alumni/settings.py
+++ Alumni/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = 'moringa-alumni.herokuapp.com'
 
 APPEND_SLASH = False
 
