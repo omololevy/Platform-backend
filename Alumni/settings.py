8b52f44b883e855d610fcfc917a4fdf975220913
omololevy <cotechlevy@gmail.com>
2022-03-25 19:42:22 +0300

    hosts config

--- Alumni/settings.py
+++ Alumni/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS=["https://my-college.herokuapp.com/", "localhost", "127.0.0.1"]
 
 APPEND_SLASH = False
 
